f512daedd16e6444aacb4ac38075f04f22f002a0
Lesroy Wedderburn <wlesroy98@outlook.com>
2025-06-23 14:09:52 +0100

    [LW] - Initial setup for chef claude course for scrimba

---
+++ scrimba_react_chef_claude/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import chefImage from '../images/Chef_icon.png'
+
+function Header() {
+
+  return (
+    <header>
+      <img src={chefImage} width='60px'/>
+      <span>Chef Claude</span>
+      </header>
+
+  )
+}
+
+export default Header
